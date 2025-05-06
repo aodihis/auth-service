d90a343d8c718bc5fbf73a2512985f4b307610d1
Iqbal <aodihis@gmail.com>
2025-05-06 15:45:20 +0700

    * Add user model

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,3 +1,4 @@
 pub mod response;
 pub mod request;
 pub mod activation_token;
+pub mod user;
---
+++ src/models/user.rs
@@ -0,0 +1,14 @@
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+
+#[derive(Serialize)]
+pub struct User {
+    pub id: Uuid,
+    pub email: String,
+    pub username: String,
+    pub is_active: bool,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
+}
