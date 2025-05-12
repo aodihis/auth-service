f408bc70359c87496fba946ed8852e83e36d285e
Iqbal <aodihis@gmail.com>
2025-05-12 09:54:58 +0700

    * Add missing route for resend token.

--- src/routes/authentication.rs
+++ src/routes/authentication.rs
@@ -1,4 +1,4 @@
-use crate::handlers::authentication::{register_user, verify_user};
+use crate::handlers::authentication::{register_user, resend_token, verify_user};
 use crate::services::authentication::Authentication as AuthenticationService;
 use axum::routing::post;
 use axum::Router;
@@ -9,5 +9,6 @@ pub fn router(auth_service: Arc<AuthenticationService>) -> Router {
     Router::new()
         .route("/register", post(register_user))
         .route("/verify", post(verify_user))
+        .route("/resend-token", post(resend_token))
         .with_state(auth_service)
 }
