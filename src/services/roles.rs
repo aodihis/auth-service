b6cb782492fde867da1f3c47e2e8dd9c51e8bf6c
Iqbal <aodihis@gmail.com>
2025-06-02 18:15:13 +0700

    * Add get and update function in roles service.

--- src/services/roles.rs
+++ src/services/roles.rs
@@ -17,6 +17,24 @@ impl Roles {
         Self { pool, config }
     }
 
+    pub async fn get(&self, id: i32) -> Result<RoleModel, AuthorizationError> {
+        let res = sqlx::query_as::<_, RoleModel>("SELECT * FROM roles WHERE id = $1")
+        .bind(id).fetch_one(&self.pool).await;
+
+        match res {
+            Ok(role)=> Ok(role),
+            Err(sqlx::Error::RowNotFound) => {
+                info!("Role not found: {}", id);
+                Err(AuthorizationError::NotFound)
+            }
+
+            Err(err) => {
+                error!("Failed to fetch role: {}", err.to_string());
+                Err(AuthorizationError::InternalServerError)
+            }
+        }
+    }
+
     pub async fn add(&self, role: Role) -> Result<RoleModel, AuthorizationError> {
         let res = sqlx::query_as::<_, RoleModel>(
             "INSERT INTO roles (name, description) VALUES ($1, $2)\
@@ -27,8 +45,8 @@ impl Roles {
             Ok(role) => Ok(role),
             Err(err) => {
                 if let sqlx::Error::Database(db_err) = &err {
-                    info!("Insert role failed - Duplicated: Role with name {}", role.name);
                     if db_err.constraint() == Some("roles_name_key") {
+                        info!("Insert role failed - Duplicated: Role with name {}", role.name);
                         return Err(AuthorizationError::RoleAlreadyExist);
                     }
                 }
@@ -54,6 +72,34 @@ impl Roles {
             }
             Err(e) => {
                 error!("Database error while deleting role id {}: {:?}", id, e);
+                Err(AuthorizationError::InternalServerError)
+            }
+        }
+    }
+
+    pub async fn update(&self, id: i32, role: Role) -> Result<RoleModel, AuthorizationError> {
+        let res = sqlx::query_as::<_, RoleModel>(
+            "UPDATE roles
+                SET name = $1, description = $2
+                WHERE id = $3
+                RETURNING id, name, description
+                "
+        ).bind(role.name.clone()).bind(role.description).bind(id).fetch_optional(&self.pool).await;
+
+        match res {
+            Ok(Some(role)) => Ok(role),
+            Ok(None) => {
+                info!("Update failed: role not found: {}", id);
+                Err(AuthorizationError::NotFound)
+            }
+            Err(err) => {
+                if let sqlx::Error::Database(db_err) = &err {
+                    if db_err.constraint() == Some("roles_name_key") {
+                        error!("Update failed: duplicate role name");
+                        return Err(AuthorizationError::RoleAlreadyExist);
+                    }
+                }
+                error!("Update role failed: {}", err.to_string());
                 Err(AuthorizationError::InternalServerError)
             }
         }
